0d913de4fb53a718276257bcbfad26ed41b02f3d
Eirik Meland <eirik.meland@arktekk.no>
2019-02-26 12:53:12 +0100

    functions for creating PathParamConverters

--- core/src/main/scala/io/unsecurity/hlinx/PathParamConverter.scala
+++ core/src/main/scala/io/unsecurity/hlinx/PathParamConverter.scala
@@ -5,6 +5,12 @@ trait PathParamConverter[A] {
   def convert(s: String): Either[String, A]
 }
 object PathParamConverter {
+  def create[A](f: String => Either[String, A]): PathParamConverter[A] =
+    (s: String) => Try(f(s)).toEither.left.map(_.getMessage).flatMap(id => id)
+
+  def createSimple[A](f: String => A): PathParamConverter[A] =
+    (s: String) => Try(f(s)).toEither.left.map(_.getMessage)
+
   def apply[A: PathParamConverter]: PathParamConverter[A] = implicitly[PathParamConverter[A]]
 
   implicit val intParamConverter: PathParamConverter[Int]       = (s: String) => Try(s.toInt).toEither.left.map(_.getMessage)
