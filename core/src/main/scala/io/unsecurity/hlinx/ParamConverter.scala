7e769c7a0ae258ee1fdace8a7c41ea7420ea9448
Eirik Meland <eirik.meland@arktekk.no>
2019-03-29 12:21:36 +0100

    urldecode when using ParamConverter.create and .createSimple

--- core/src/main/scala/io/unsecurity/hlinx/ParamConverter.scala
+++ core/src/main/scala/io/unsecurity/hlinx/ParamConverter.scala
@@ -1,5 +1,7 @@
 package io.unsecurity.hlinx
 
+import java.net.URLDecoder
+
 import scala.util.Try
 
 //trait Converter[A, B] {
@@ -24,15 +26,33 @@ trait ParamConverter[A] {
   }
 }
 object ParamConverter {
-  def create[A](f: String => Either[String, A]): ParamConverter[A] =
-    (s: String) => Try(f(s)).toEither.left.map(_.getMessage).flatMap(id => id)
+  // TODO use stringParamConverter as base for every creation of paramconverters (to include url decoding)
+  implicit val stringParamConverter: ParamConverter[String] = (s: String) =>
+    Try {
+      URLDecoder.decode(s, "UTF-8")
+    }.toEither.left.map(_.getMessage)
+
+  implicit val intParamConverter: ParamConverter[Int] = (s: String) =>
+    Try {
+      val decoded = URLDecoder.decode(s, "UTF-8")
+      decoded.toInt
+    }.toEither.left.map(_.getMessage)
+
+  def create[A](f: String => Either[String, A]): ParamConverter[A] = { (s: String) =>
+    Try {
+      val decoded: String = URLDecoder.decode(s, "UTF-8")
+      f(decoded)
+    }.toEither.left.map(_.getMessage).flatMap(id => id)
+  }
 
   def createSimple[A](f: String => A): ParamConverter[A] =
-    (s: String) => Try(f(s)).toEither.left.map(_.getMessage)
+    (s: String) =>
+      Try {
+        val decoded: String = URLDecoder.decode(s, "UTF-8")
+        f(decoded)
+      }.toEither.left.map(_.getMessage)
 
   def apply[A: ParamConverter]: ParamConverter[A] = implicitly[ParamConverter[A]]
 
-  implicit val intParamConverter: ParamConverter[Int]       = (s: String) => Try(s.toInt).toEither.left.map(_.getMessage)
-  implicit val stringParamConverter: ParamConverter[String] = (s: String) => Right(s)
 }
 case class Param[A: ParamConverter](name: String, converter: ParamConverter[A])
