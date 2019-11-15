5a27d5147007c481ae135766da1c692c8163f313
Morten Andersen-Gott <morten.andersengott@gmail.com>
2019-11-15 14:34:47 +0100

    New tests

--- core/src/test/scala/io/unsecurity/ConNegTest.scala
+++ core/src/test/scala/io/unsecurity/ConNegTest.scala
@@ -1,16 +1,15 @@
 package io.unsecurity
 
-import cats.Id
 import cats.effect.IO
+import cats.implicits._
 import no.scalabin.http4s.directives.Result
 import org.http4s.{Header, Headers, MediaRange, Method, Request, _}
 import org.scalatest.EitherValues._
 import org.scalatest.OptionValues._
 import org.scalatest.{FlatSpec, Matchers}
 import org.slf4j.{Logger, LoggerFactory}
-import cats.implicits._
 
-class ConNegTest extends FlatSpec with Matchers {
+class ConNegTest extends IOFunSuite with Matchers {
 
   private val vndMediaType: MediaRange       = MediaRange.parse("application/vnd.testing+json").right.get
   val supportedContentTypes: Set[MediaRange] = Set(vndMediaType)
@@ -20,35 +19,60 @@ class ConNegTest extends FlatSpec with Matchers {
     override def log: Logger                             = LoggerFactory.getLogger("fjon")
   }
 
-  "Invalid content type" should "return Invalid Media-Type error" in {
+  test("Invalid content type should return Invalid Media-Type error") = {
     val req =
       Request[IO](method = Method.GET, uri = uri"/whatever", headers = Headers.of(Header("content-type", "foobar")))
 
-    val result: Result[IO, MediaRange] = unsecurity.validateContentType(supportedContentTypes).run(req).unsafeRunSync()
-
-    result match {
-      case Result.Error(response) =>
-        assert(response.status == Status.UnsupportedMediaType)
-        val body: EntityBody[IO] = response.body
-        val t: String = body.through(fs2.text.utf8Decode).compile.foldMonoid.unsafeRunSync()
-        assert(t.contains("Content-Type missing or invalid mediatype"))
+    for {
+      result <- unsecurity.validateContentType(supportedContentTypes).run(req)
+      body <- result match {
+               case Result.Error(response) =>
+                 assert(response.status == Status.UnsupportedMediaType)
+                 response.body
+                   .through(fs2.text.utf8Decode)
+                   .compile
+                   .foldMonoid
+               case _ => fail("ouch")
+             }
+    } yield {
+      assert(body.contains("Content-Type missing or invalid mediatype"))
     }
   }
 
-  "Unsupported content-type" should "return Usupported Media-Type error" in {
+  test("Unsupported Content-Type should return Invalid Media-Type error") = {
+
     val req = Request[IO](method = Method.GET,
-                          uri = uri"/whatever",
-                          headers = Headers.of(Header("content-type", "application/foobar")))
-    val result = Unsecurity.validateContentType(req, supportedContentTypes)
-    result.left.value.detail.value should startWith("Content-Type not supported")
+      uri = uri"/whatever",
+      headers = Headers.of(Header("content-type", "application/foobar")))
+
+    for {
+      result <- unsecurity.validateContentType(supportedContentTypes).run(req)
+      body <- result match {
+        case Result.Failure(response) =>
+          assert(response.status == Status.UnsupportedMediaType)
+          response.body
+            .through(fs2.text.utf8Decode)
+            .compile
+            .foldMonoid
+        case _ =>
+          fail("ouch")
+      }
+    } yield {
+      assert(body.contains("Content-Type not supported"))
+    }
   }
 
-  "Supported content-type" should "return MediaType" in {
+  test("Supported content-type should return mediatype") = {
     val req = Request[IO](method = Method.GET,
-                          uri = uri"/whatever",
-                          headers = Headers.of(Header("content-type", "application/vnd.testing+json")))
-    val result = Unsecurity.validateContentType(req, supportedContentTypes)
-    result.right.value shouldBe (vndMediaType)
+      uri = uri"/whatever",
+      headers = Headers.of(Header("content-type", "application/vnd.testing+json")))
+    for {
+      result <- unsecurity.validateContentType(supportedContentTypes).run(req)
+    } yield {
+      assert(result == Result.Success(vndMediaType))
+    }
   }
+
+
 
 }
