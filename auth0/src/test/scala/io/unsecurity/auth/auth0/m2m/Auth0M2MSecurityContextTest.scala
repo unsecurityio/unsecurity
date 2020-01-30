bcc60e3db7cefc6e5919d19c9b4b38c566835c8e
Morten Andersen-Gott <morten.andersengott@gmail.com>
2020-01-30 15:12:23 +0100

    On behalf of (#60)
    * Support multiple authorization headers
    
    * Support On-Behalf-Of authroization scheme

---
+++ auth0/src/test/scala/io/unsecurity/auth/auth0/m2m/Auth0M2MSecurityContextTest.scala
@@ -0,0 +1,26 @@
+package io.unsecurity.auth.auth0.m2m
+
+import java.util.UUID
+
+import cats.effect.IO
+import com.auth0.jwk.{Jwk, JwkProvider}
+import io.unsecurity.auth.auth0.UnsecurityTestSuite
+import no.scalabin.http4s.directives.Result
+import org.http4s.{Request, _}
+
+class Auth0M2MSecurityContextTest extends UnsecurityTestSuite{
+
+  val m2mSecurity = new Auth0M2MSecurityContext[IO, Nothing](_ => ???, "issuer", "aud", new FdUpJwkProvider)
+
+  test("Identifies Bearer token when multiple Authorization headers set") {
+    val get = Request[IO](headers = Headers.of(Header("Authorization", "M2M Don't say you love me, you don't even know me"), Header("Authorization", s"Bearer ${UUID.randomUUID().toString}")))
+    m2mSecurity.requestAuthToken.run(get).unsafeRunSync().where{
+      case Result.Success(token) => Ok
+    }
+  }
+
+  class FdUpJwkProvider extends JwkProvider {
+    override def get(keyId: String): Jwk = ???
+  }
+
+}
